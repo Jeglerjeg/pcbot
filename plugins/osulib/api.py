85fd7e5ccdfe77d3beaa60f6af652f89ef7e6f63
Jeglet <jegler@heckin.how>
2021-07-19 21:42:45 +0200

    Make / between set id and mode optional in v2 beatmapset regex

--- plugins/osulib/api.py
+++ plugins/osulib/api.py
@@ -311,7 +311,7 @@ async def get_user_recent_activity(user, params=None):
 
 beatmap_url_pattern_v1 = re.compile(r"https?://(osu|old)\.ppy\.sh/(?P<type>[bs])/(?P<id>\d+)(?:\?m=(?P<mode>\d))?")
 beatmapset_url_pattern_v2 = \
-    re.compile(r"https?://osu\.ppy\.sh/beatmapsets/(?P<beatmapset_id>\d+)/(?:#(?P<mode>\w+)/(?P<beatmap_id>\d+))?")
+    re.compile(r"https?://osu\.ppy\.sh/beatmapsets/(?P<beatmapset_id>\d+)/?(?:#(?P<mode>\w+)/(?P<beatmap_id>\d+))?")
 beatmap_url_pattern_v2 = re.compile(r"https?://osu\.ppy\.sh/beatmaps/(?P<beatmap_id>\d+)(?:\?mode=(?P<mode>\w+))?")
 
 BeatmapURLInfo = namedtuple("BeatmapURLInfo", "beatmapset_id beatmap_id gamemode")
