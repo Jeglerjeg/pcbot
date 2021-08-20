35657c40d5b05344f0a7448de5e8f2b3028b57c7
Jeglet <jegler@heckin.how>
2021-08-20 13:57:15 +0200

    Change previous update time to UTC for consistency with time_started

--- plugins/osu.py
+++ plugins/osu.py
@@ -1093,7 +1093,7 @@ async def on_ready():
         finally:
             # Save the time elapsed since we started the update
             time_elapsed = (datetime.now() - started).total_seconds()
-            previous_update = datetime.now()
+            previous_update = datetime.utcnow()
 
 
 async def on_reload(name: str):
