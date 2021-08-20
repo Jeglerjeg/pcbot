e980e1df44a46952e35bf485fcff5b24bd505223
Jeglet <jegler@heckin.how>
2021-08-20 23:32:53 +0200

    Rename gamemode enums

--- plugins/osulib/api.py
+++ plugins/osulib/api.py
@@ -22,10 +22,10 @@ mapcache_path = "plugins/osulib/mapdatacache"
 setcache_path = "plugins/osulib/setdatacache"
 
 mode_names = {
-    "Standard": ["standard", "osu", "std"],
-    "Taiko": ["taiko"],
-    "Catch": ["catch", "ctb", "fruits"],
-    "Mania": ["mania", "keys"]
+    "osu": ["standard", "osu", "std"],
+    "taiko": ["taiko"],
+    "catch": ["catch", "ctb", "fruits"],
+    "mania": ["mania", "keys"]
 }
 
 
@@ -59,10 +59,10 @@ async def get_access_token(client_id, client_secret):
 
 class GameMode(Enum):
     """ Enum for gamemodes. """
-    Standard = 0
-    Taiko = 1
-    Catch = 2
-    Mania = 3
+    osu = 0
+    taiko = 1
+    catch = 2
+    mania = 3
 
     @classmethod
     def get_mode(cls, mode: str):
@@ -371,7 +371,6 @@ def parse_beatmap_url(url: str):
         if match_v2_beatmapset.group("mode") is None:
             return BeatmapURLInfo(beatmapset_id=match_v2_beatmapset.group("beatmapset_id"), beatmap_id=None,
                                   gamemode=None)
-
         return BeatmapURLInfo(beatmapset_id=match_v2_beatmapset.group("beatmapset_id"),
                               beatmap_id=match_v2_beatmapset.group("beatmap_id"),
                               gamemode=GameMode.get_mode(match_v2_beatmapset.group("mode")))
