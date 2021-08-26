73a48f45b8295ff9219bfbba2ab254fe469cc965
Jeglet <jegler@heckin.how>
2021-08-26 14:12:46 +0200

    Remove ranked score when simulating score

--- plugins/osu.py
+++ plugins/osu.py
@@ -1752,6 +1752,7 @@ async def score(message: discord.Message, *options):
     if mods:
         osu_score["mods"] = wrap(mods, 2)
         osu_score["pp"] = None
+        osu_score["score"] = None
         scoreboard_rank = None
     else:
         scoreboard_rank = osu_scores["position"]
