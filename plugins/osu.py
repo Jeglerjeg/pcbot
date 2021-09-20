a83d53039b80eed83df8e30242ff8c0a6a5e27b0
Jeglet <jegler@heckin.how>
2021-09-20 20:16:33 +0200

    Remove unused variable and fix a few warnings

--- plugins/osu.py
+++ plugins/osu.py
@@ -420,7 +420,7 @@ def get_formatted_score_time(osu_score: dict):
     return time_string
 
 
-def get_beatmap_sr(score_pp: PPStats, beatmap: dict, mode: api.GameMode, mods: str):
+def get_beatmap_sr(score_pp: PPStats, beatmap: dict, mods: str):
     """ Change beatmap SR if using SR adjusting mods. """
     difficulty_rating = score_pp.stars \
         if mods not in ("Nomod", "HD", "FL", "TD", "ScoreV2", "NF", "SD", "PF", "RX") \
@@ -625,7 +625,7 @@ async def get_formatted_score_list(member: discord.Member, osu_scores: list, lim
         beatmap = (await api.beatmap_lookup(params=params, map_id=osu_score["beatmap"]["id"], mode=mode.name))
         score_pp = await get_score_pp(osu_score, mode, beatmap)
         if score_pp is not None:
-            beatmap["difficulty_rating"] = get_beatmap_sr(score_pp, beatmap, mode, mods)
+            beatmap["difficulty_rating"] = get_beatmap_sr(score_pp, beatmap, mods)
 
         # Add time since play to the score
         score_datetime = datetime.fromisoformat(osu_score["created_at"])
@@ -847,7 +847,7 @@ async def notify_pp(member_id: str, data: dict):
         # Calculate PP and change beatmap SR if using a difficult adjusting mod
         potential_pp = await get_score_pp(osu_score, mode, beatmap)
         mods = Mods.format_mods(osu_score["mods"])
-        beatmap["difficulty_rating"] = get_beatmap_sr(potential_pp, beatmap, mode, mods)
+        beatmap["difficulty_rating"] = get_beatmap_sr(potential_pp, beatmap, mods)
         if update_mode is UpdateModes.Minimal:
             m.append("".join([await format_minimal_score(mode, osu_score, beatmap, scoreboard_rank, member), "\n"]))
         else:
@@ -1508,8 +1508,8 @@ async def leaderboard_notifications(message: discord.Message, notify_setting: st
     member = message.author
     # Make sure the member is assigned
     assert str(member.id) in osu_config.data["profiles"], \
-    "No osu! profile assigned to **{}**! Please assign a profile using {}osu link".format(
-        member.name, botconfig.guild_command_prefix(member.guild))
+        "No osu! profile assigned to **{}**! Please assign a profile using {}osu link".format(
+            member.name, botconfig.guild_command_prefix(member.guild))
 
     if notify_setting.lower() == "on":
         osu_config.data["leaderboard"][str(member.id)] = True
@@ -1651,7 +1651,7 @@ async def create_score_embed_with_pp(member: discord.Member, osu_score: dict, be
     elif osu_score["pp"] is None:
         osu_score["pp"] = 0
     if score_pp is not None:
-        beatmap["difficulty_rating"] = get_beatmap_sr(score_pp, beatmap, mode, mods)
+        beatmap["difficulty_rating"] = get_beatmap_sr(score_pp, beatmap, mods)
 
     # There might not be any events
     if scoreboard_rank is False and str(member.id) in osu_tracking and "new" in osu_tracking[str(member.id)] \
@@ -1905,7 +1905,6 @@ def calculate_total_user_pp(osu_scores: list, member_id: str):
 @osu.command(aliases="configure cfg")
 async def config(message, _: utils.placeholder):
     """ Manage configuration for this plugin. """
-    pass
 
 
 @config.command(alias="score", permissions="manage_guild")
