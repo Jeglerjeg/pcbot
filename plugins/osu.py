8a924a8f26a997285f5c696609bea3e3f977ff21
Jeglet <jegler@heckin.how>
2021-09-24 04:33:06 +0200

    Fix osu user sometimes getting data from the wrong guild

--- plugins/osu.py
+++ plugins/osu.py
@@ -1063,7 +1063,7 @@ def get_user(message: discord.Message, username: str):
     if not member:
         for data in osu_tracking.values():
             if data["new"]["username"].lower() == username.lower():
-                member = data["member"]
+                member = utils.find_member(guild=message.guild, name=data["member"].name)
     return member
 
 
@@ -1823,7 +1823,6 @@ async def create_score_embed_with_pp(member: discord.Member, osu_score: dict, be
 
 async def recent(message: discord.Message, user: str):
     """ Display your or another member's most recent score. """
-    logging.info(user)
     member = get_user(message, user)
     if not member:
         member = message.author
