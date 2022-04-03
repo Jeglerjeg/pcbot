5362d1f2fd5d093cbe24850994fa31089b87bda3
jeglerjeg <jegler@heckin.how>
2022-04-03 14:45:12 +0200

    Only check is_nsfw() if channel is a text channel

--- plugins/moderate.py
+++ plugins/moderate.py
@@ -219,7 +219,8 @@ async def check_nsfw(message: discord.Message):
         return False
 
     # Do not check if the channel is designed for nsfw content
-    if "nsfw" in message.channel.name or message.channel.is_nsfw():
+    if "nsfw" in message.channel.name or \
+            isinstance(message.channel, discord.abc.TextChannel) and message.channel.is_nsfw():
         return False
 
     # Check if message includes keyword nsfw and a link
