03a10c090348918d8fc7acc7c292920d10172111
Jeglerjeg <jegler@heckin.how>
2022-07-01 21:36:34 +0200

    Try tweaking music player settings to avoid audio skipping

--- plugins/music.py
+++ plugins/music.py
@@ -42,7 +42,7 @@ music_channels = Config("music_channels", data=[])
 voice_states = {}  # type: Dict[discord.Guild, VoiceState]
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'extractaudio:': True,
+    'audioformat': 'opus',
     'noplaylist': True,
     'nocheckcertificate': True,
     'quiet': True,
@@ -51,7 +51,7 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    # 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn',
 }
 
