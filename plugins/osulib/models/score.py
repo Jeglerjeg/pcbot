37653a047a53f59e0067e7e4b059f418e9a4cc5e
Jeglerjeg <jegler@heckin.how>
2024-03-04 19:06:48 +0100

    Fix mania maximum_statistics

--- plugins/osulib/models/score.py
+++ plugins/osulib/models/score.py
@@ -37,12 +37,14 @@ class ScoreStatistics:
 
 
 class MaximumScoreStatistics:
+    perfect: int
     great: int
     large_tick_hit: int
     legacy_combo_increase: int
     ignore_hit: int
 
     def __init__(self, raw_data: dict):
+        self.perfect = raw_data["perfect"] if "perfect" in raw_data else 0
         self.great = raw_data["great"] if "great" in raw_data else 0
         self.large_tick_hit = raw_data["large_tick_hit"] if "large_tick_hit" in raw_data else 0
         self.legacy_combo_increase = raw_data["legacy_combo_increase"] if "legacy_combo_increase" in raw_data else 0
